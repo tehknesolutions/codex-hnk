1ec1ae175db9f13a36364f3065b4525287c26ae2
tehknesolutions <tehknesolutions@gmail.com>
2026-09-07 14:45:40 -0300

    chore(migration): restore database exports

---
+++ packages/database/src/index.ts
@@ -0,0 +1,10 @@
+export type { Json } from './database.types';
+export type {
+  Database,
+  Insert,
+  PublicTables,
+  Row,
+  TableName,
+  Update,
+  VaultKeyEnvelopesTable,
+} from './database.vault-key-envelopes.generated';
