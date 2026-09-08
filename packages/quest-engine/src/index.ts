e7da533a5af00bf9c6f67f9f0e53d429c8986575
tehknesolutions <tehknesolutions@gmail.com>
2026-09-08 13:39:40 -0300

    feat(quest-engine): export quest registry

--- packages/quest-engine/src/index.ts
+++ packages/quest-engine/src/index.ts
@@ -3,3 +3,4 @@ export * from "./experience-director.js";
 export * from "./renderer-registry.js";
 export * from "./render-surface.js";
 export * from "./runtime.js";
+export * from "./registry.js";
