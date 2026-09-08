b87dd412119a474363201b122de580bfffe4ed9f
tehknesolutions <tehknesolutions@gmail.com>
2026-09-08 16:21:38 -0300

    refactor(web): expose reusable HNK Web runtime aliases

---
+++ apps/web/app/_runtime/WebHnkRuntime.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+export {
+  WebDay001RuntimeProvider as WebHnkRuntimeProvider,
+  WebAtriumBoundary,
+  useWebDay001Runtime as useWebHnkRuntime,
+  type WebDay001RuntimeState as WebHnkRuntimeState,
+} from '../day-001/WebDay001Runtime';
