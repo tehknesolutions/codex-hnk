b3bc4b0216f8bbe8966f83fa23cd85a3f078d822
tehknesolutions <tehknesolutions@gmail.com>
2026-09-08 16:25:26 -0300

    feat(mobile): expose Day 002 Quest-powered route

---
+++ apps/mobile/src/app/day-002.tsx
@@ -0,0 +1,10 @@
+import { AtriumGate } from '../features/auth/AtriumGate';
+import { Day002GoldenV1Mobile } from '../features/kether/Day002GoldenV1Mobile';
+
+export default function Day002Screen() {
+  return (
+    <AtriumGate>
+      <Day002GoldenV1Mobile />
+    </AtriumGate>
+  );
+}
