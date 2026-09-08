3eb1c9cc1c04f643740f3d8ec424ab13235e4efb
tehknesolutions <tehknesolutions@gmail.com>
2026-09-08 19:31:41 -0300

    feat(mobile): add Day 004 route

---
+++ apps/mobile/src/app/day-004.tsx
@@ -0,0 +1,10 @@
+import { AtriumGate } from '../features/auth/AtriumGate';
+import { Day004GoldenV1Mobile } from '../features/kether/Day004GoldenV1Mobile';
+
+export default function Day004Screen() {
+  return (
+    <AtriumGate>
+      <Day004GoldenV1Mobile />
+    </AtriumGate>
+  );
+}
