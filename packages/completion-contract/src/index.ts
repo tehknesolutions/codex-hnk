cbe7c63ea00beddc2e2a3e297aa93afb3afef4ff
tehknesolutions <tehknesolutions@gmail.com>
2026-09-09 11:01:21 -0300

    chore(day009): export completion contract

--- packages/completion-contract/src/index.ts
+++ packages/completion-contract/src/index.ts
@@ -8,5 +8,6 @@ export * from "./day005.js";
 export * from "./day006.js";
 export * from "./day007.js";
 export * from "./day008.js";
+export * from "./day009.js";
 export * from "./service.js";
 export * from "./offline.js";
