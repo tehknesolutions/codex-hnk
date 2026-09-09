6e7f0a0cc192ce6615ff4a8665dffd887d8de5a8
tehknesolutions <tehknesolutions@gmail.com>
2026-09-09 20:05:35 -0300

    feat(day015): export completion contract

--- packages/completion-contract/src/index.ts
+++ packages/completion-contract/src/index.ts
@@ -14,5 +14,6 @@ export * from "./day011.js";
 export * from "./day012.js";
 export * from "./day013.js";
 export * from "./day014.js";
+export * from "./day015.js";
 export * from "./service.js";
 export * from "./offline.js";
