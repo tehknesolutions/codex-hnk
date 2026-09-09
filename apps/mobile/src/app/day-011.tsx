ef4834f91fdc43b75c510a910a78fdc2550178fa
tehknesolutions <tehknesolutions@gmail.com>
2026-09-09 14:11:44 -0300

    feat(day011): expose Expo route

---
+++ apps/mobile/src/app/day-011.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day011GoldenV1Mobile} from '../features/kether/Day011GoldenV1Mobile';
+export default function Day011Screen(){return <AtriumGate><Day011GoldenV1Mobile/></AtriumGate>}
