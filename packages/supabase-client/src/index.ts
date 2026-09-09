84736ef82c6a7c0758747119b17eb751289b631a
tehknesolutions <tehknesolutions@gmail.com>
2026-09-09 17:43:42 -0300

    feat(day013): export Number Crusher client

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -18,6 +18,7 @@ export * from './day009-v1';
 export * from './day010-v1';
 export * from './day011-v1';
 export * from './day012-v1';
+export * from './day013-v1';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
