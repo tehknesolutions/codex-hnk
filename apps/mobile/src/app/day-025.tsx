480f6e64a0bec02b8e30cb6bbf16a7edd8610d23
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 16:21:13 -0300

    feat(day025): expose native AR route

---
+++ apps/mobile/src/app/day-025.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day025GoldenV1Mobile} from '../features/kether/Day025GoldenV1Mobile';
+export default function Day025Route(){return <AtriumGate><Day025GoldenV1Mobile/></AtriumGate>}
