2f373042bd2517482731717f82b80aec7ff5a7fe
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 18:55:07 -0300

    feat(day028): add Expo route

---
+++ apps/mobile/src/app/day-028.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day028GoldenV1Mobile} from '../features/kether/Day028GoldenV1Mobile';
+export default function Day028Route(){return <AtriumGate><Day028GoldenV1Mobile/></AtriumGate>}
