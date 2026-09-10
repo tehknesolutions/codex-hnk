10dda5f2dc58109d870e5cda132164459ce860e8
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 18:47:41 -0300

    feat(day028): export Gneo Geo client

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -33,6 +33,7 @@ export * from './day024-v1';
 export * from './day025-v1';
 export * from './day026-v1';
 export * from './day027-v1';
+export * from './day028-v1';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
