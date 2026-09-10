f42198c9e85717fb3923bbaccb18feaff82e32b9
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 20:19:55 -0300

    feat(day030): add protected Expo route

---
+++ apps/mobile/src/app/day-030.tsx
@@ -0,0 +1,6 @@
+import { AtriumGate } from '../features/kether/AtriumGate';
+import { Day030GoldenV1Mobile } from '../features/kether/Day030GoldenV1Mobile';
+
+export default function Day030Route() {
+  return <AtriumGate><Day030GoldenV1Mobile /></AtriumGate>;
+}
