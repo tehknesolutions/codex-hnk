bb603a3d4f0fa70e5caf0de8ccbfe52cc1ec7526
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 16:12:14 -0300

    feat(ar): export Day025 canonical geometry

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,4 +1,5 @@
 export * from './board';
 export * from './catalog';
 export * from './kether-references';
+export * from './day025-dai-koo-myo-geometry';
 export { ketherChapterOverview } from './boards/kether-chapter-overview';
