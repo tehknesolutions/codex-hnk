0ae2e7c2bcebe7478ac89bf887cdc3e3c06d4d37
tehknesolutions <tehknesolutions@gmail.com>
2026-09-10 12:55:57 -0300

    feat(day020): export Supabase client

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -25,6 +25,7 @@ export * from './day016-v1';
 export * from './day017-v1';
 export * from './day018-v1';
 export * from './day019-v1';
+export * from './day020-v1';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
