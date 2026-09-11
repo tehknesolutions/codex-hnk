0fe7b62955f80a4461203519817cee5302cd1038
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 16:55:58 -0300

    feat(day037): route Cahetel Expo runtime behind AtriumGate

---
+++ apps/mobile/src/app/day-037.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day037GoldenV1Mobile} from '../features/chokmah/Day037GoldenV1Mobile';
+export default function Day037Route(){return <AtriumGate><Day037GoldenV1Mobile/></AtriumGate>}
