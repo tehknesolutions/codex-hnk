0c4efbe4dacb52479d3c32f8b38b5b0e30b11ac4
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 11:25:31 -0300

    feat(day031): add mobile route

---
+++ apps/mobile/src/app/day-031.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day031GoldenV1Mobile} from '../features/kether/Day031GoldenV1Mobile';
+export default function Day031Route(){return <AtriumGate><Day031GoldenV1Mobile/></AtriumGate>}
