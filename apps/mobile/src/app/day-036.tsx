fc724a448725575cf408a8cf431598a777dfae37
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 15:28:34 -0300

    feat(day036): expose locked Expo portal readiness route

---
+++ apps/mobile/src/app/day-036.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day036PortalReadinessMobile} from '../features/kether/Day036PortalReadinessMobile';
+export default function Day036Route(){return <AtriumGate><Day036PortalReadinessMobile/></AtriumGate>}
