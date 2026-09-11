e6050045de7fd510cc83808d4075908306d5acef
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 11:40:37 -0300

    feat(day032): export elevator Supabase client

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -37,6 +37,7 @@ export * from './day028-v1';
 export * from './day029-v1';
 export * from './day030-v1';
 export * from './day031-v1';
+export * from './day032-v1';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
