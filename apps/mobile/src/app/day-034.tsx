e7940dffed6c8b9bcfae4c5571662400795b8fa7
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 14:09:10 -0300

    feat(day034): expose Expo runtime route

---
+++ apps/mobile/src/app/day-034.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day034GoldenV1Mobile} from '../features/kether/Day034GoldenV1Mobile';
+export default function Day034Route(){return <AtriumGate><Day034GoldenV1Mobile/></AtriumGate>}
