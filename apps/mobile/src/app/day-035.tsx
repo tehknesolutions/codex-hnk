a284dcb6cead04a06b79d9011bf41ff5ba774559
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 14:26:06 -0300

    feat(day035): expose Expo cycle-seal route

---
+++ apps/mobile/src/app/day-035.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day035GoldenV1Mobile} from '../features/kether/Day035GoldenV1Mobile';
+export default function Day035Route(){return <AtriumGate><Day035GoldenV1Mobile/></AtriumGate>}
