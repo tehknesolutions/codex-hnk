7752d476fdefa2a2a3cd17760c87582f09136344
tehknesolutions <tehknesolutions@gmail.com>
2026-09-12 16:02:47 -0300

    feat(day046): route Haziel V mobile runtime

---
+++ apps/mobile/src/app/day-046.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day046GoldenV2Mobile} from '../features/chokmah/Day046GoldenV2Mobile';
+export default function Day046Route(){return <AtriumGate><Day046GoldenV2Mobile/></AtriumGate>}
