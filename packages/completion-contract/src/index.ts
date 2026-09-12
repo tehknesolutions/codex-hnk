b827b42d6da917b853b217edb71bbc6798649fc1
tehknesolutions <tehknesolutions@gmail.com>
2026-09-12 18:05:51 -0300

    feat(day048): export Aladiah II completion contract

--- packages/completion-contract/src/index.ts
+++ packages/completion-contract/src/index.ts
@@ -47,5 +47,6 @@ export * from "./day044.js";
 export * from "./day045.js";
 export * from "./day046.js";
 export * from "./day047.js";
+export * from "./day048.js";
 export * from "./service.js";
 export * from "./offline.js";
