17eb2ea68e39effcb1055af1679adeed15151ff5
tehknesolutions <tehknesolutions@gmail.com>
2026-09-11 21:13:41 -0300

    day041: export Supabase client v2

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -46,6 +46,7 @@ export * from './day037-v1';
 export * from './day038-v1';
 export * from './day039-v1';
 export * from './day040-v2';
+export * from './day041-v2';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
