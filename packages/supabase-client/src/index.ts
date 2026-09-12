00befbd1de4f4ef419e6c73f67dec7a0f905a952
tehknesolutions <tehknesolutions@gmail.com>
2026-09-12 18:10:09 -0300

    feat(day048): export Aladiah II Supabase client

--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -52,6 +52,7 @@ export * from './day043-v2';
 export * from './day044-v2';
 export * from './day046-v2';
 export * from './day047-v2';
+export * from './day048-v2';
 export * from './kether-cycle01';
 export * from './kether-cycle02';
 export * from './kether-cycle03';
