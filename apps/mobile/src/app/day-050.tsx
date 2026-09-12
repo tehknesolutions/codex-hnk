39c31e88aa45914c6d956d142125ad2bd0ec634a
tehknesolutions <tehknesolutions@gmail.com>
2026-09-12 19:08:07 -0300

    route Day 050 Expo

---
+++ apps/mobile/src/app/day-050.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day050GoldenV2Mobile} from '../features/chokmah/Day050GoldenV2Mobile';
+export default function Day050Route(){return <AtriumGate><Day050GoldenV2Mobile/></AtriumGate>}
