55907bcddb9502300236c838b6796b9c8891730c
tehknesolutions <tehknesolutions@gmail.com>
2026-09-12 15:46:48 -0300

    feat(day045): route locked mobile audio readiness page

---
+++ apps/mobile/src/app/day-045.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day045AudioReadinessMobile} from '../features/chokmah/Day045AudioReadinessMobile';
+export default function Day045Route(){return <AtriumGate><Day045AudioReadinessMobile/></AtriumGate>}
