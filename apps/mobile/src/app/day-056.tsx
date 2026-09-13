6c218163619062c97c526ddd84856f86e347f7fb
tehknesolutions <tehknesolutions@gmail.com>
2026-09-13 12:50:45 -0300

    route Day056 Expo runtime

---
+++ apps/mobile/src/app/day-056.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day056GoldenV2Mobile} from '../features/chokmah/Day056GoldenV2Mobile';
+export default function Day056Route(){return <AtriumGate><Day056GoldenV2Mobile/></AtriumGate>}
