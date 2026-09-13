b671fb8b2877276ace2c0605c59d4ba63756bbb8
tehknesolutions <tehknesolutions@gmail.com>
2026-09-13 18:56:16 -0300

    add Day062 Expo route

---
+++ apps/mobile/src/app/day-062.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day062GoldenV2Mobile} from '../features/chokmah/Day062GoldenV2Mobile';
+export default function Day062Route(){return <AtriumGate><Day062GoldenV2Mobile/></AtriumGate>}
