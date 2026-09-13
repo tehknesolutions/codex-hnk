7cc3f507a5d6772786301c0ff027b3df6ddc9ae3
tehknesolutions <tehknesolutions@gmail.com>
2026-09-13 20:22:01 -0300

    add Day064 mobile route

---
+++ apps/mobile/src/app/day-064.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/kether/AtriumGate';
+import {Day064GoldenV2Mobile} from '../features/chokmah/Day064GoldenV2Mobile';
+export default function Day064Route(){return <AtriumGate><Day064GoldenV2Mobile/></AtriumGate>}
