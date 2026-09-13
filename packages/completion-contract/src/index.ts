bf61bd3c25b612a07925c41274b999faa063c1b0
tehknesolutions <tehknesolutions@gmail.com>
2026-09-13 14:19:13 -0300

    export Day058 completion contract

--- packages/completion-contract/src/index.ts
+++ packages/completion-contract/src/index.ts
@@ -57,5 +57,6 @@ export * from "./day054.js";
 export * from "./day055.js";
 export * from "./day056.js";
 export * from "./day057.js";
+export * from "./day058.js";
 export * from "./service.js";
 export * from "./offline.js";
