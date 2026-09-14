f27a470b8c812945547b87795d2d3825a9adf3c0
tehknesolutions <tehknesolutions@gmail.com>
2026-09-14 11:41:50 -0300

    add Day069 mobile route

---
+++ apps/mobile/src/app/day-069.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day069GoldenV2Mobile} from '../features/chokmah/Day069GoldenV2Mobile';
+export default function Day069Route(){return <AtriumGate><Day069GoldenV2Mobile/></AtriumGate>}
