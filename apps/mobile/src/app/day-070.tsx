03bff3ba1f3ef7dac09e45b372619aaa0554df85
tehknesolutions <tehknesolutions@gmail.com>
2026-09-14 14:41:11 -0300

    route Day070 Expo to active runtime

--- apps/mobile/src/app/day-070.tsx
+++ apps/mobile/src/app/day-070.tsx
@@ -1,3 +1,3 @@
 import {AtriumGate} from '../features/auth/AtriumGate';
-import {Day070ReferenceLockedMobile} from '../features/chokmah/Day070ReferenceLockedMobile';
-export default function Day070Route(){return <AtriumGate><Day070ReferenceLockedMobile/></AtriumGate>}
+import {Day070GoldenV2Mobile} from '../features/chokmah/Day070GoldenV2Mobile';
+export default function Day070Route(){return <AtriumGate><Day070GoldenV2Mobile/></AtriumGate>}
