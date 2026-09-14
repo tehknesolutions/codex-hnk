44f43e2a74632744c6a55b741c2933e329076f45
tehknesolutions <tehknesolutions@gmail.com>
2026-09-14 12:21:38 -0300

    add Day070 locked mobile route

---
+++ apps/mobile/src/app/day-070.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day070ReferenceLockedMobile} from '../features/chokmah/Day070ReferenceLockedMobile';
+export default function Day070Route(){return <AtriumGate><Day070ReferenceLockedMobile/></AtriumGate>}
