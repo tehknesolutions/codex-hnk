573d15745c6f4bee3d0adbd9548bfb62a4edb476
tehknesolutions <tehknesolutions@gmail.com>
2026-09-14 15:46:18 -0300

    add Day071 Expo route

---
+++ apps/mobile/src/app/day-071.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day071GoldenV2Mobile} from '../features/chokmah/Day071GoldenV2Mobile';
+export default function Day071Route(){return <AtriumGate><Day071GoldenV2Mobile/></AtriumGate>}
