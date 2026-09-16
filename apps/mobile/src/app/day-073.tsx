26871acef224771284e656746ba5c06f08a3692b
tehknesolutions <tehknesolutions@gmail.com>
2026-09-16 19:51:08 -0300

    feat(portal073): route day 073 to fail-closed readiness

---
+++ apps/mobile/src/app/day-073.tsx
@@ -0,0 +1,3 @@
+import {AtriumGate} from '../features/auth/AtriumGate';
+import {Day073PortalReadinessMobile} from '../features/chokmah/Day073PortalReadinessMobile';
+export default function Day073Route(){return <AtriumGate><Day073PortalReadinessMobile/></AtriumGate>}
