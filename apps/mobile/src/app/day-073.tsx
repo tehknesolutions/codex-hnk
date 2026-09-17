e564bdd7c07c60fc2caccca7a560d7cf824a3d4e
tehknesolutions <tehknesolutions@gmail.com>
2026-09-16 21:13:28 -0300

    feat(portal073): bind day 073 route to interactive fail-closed experience

--- apps/mobile/src/app/day-073.tsx
+++ apps/mobile/src/app/day-073.tsx
@@ -1,3 +1,3 @@
 import {AtriumGate} from '../features/auth/AtriumGate';
-import {Day073PortalReadinessMobile} from '../features/chokmah/Day073PortalReadinessMobile';
-export default function Day073Route(){return <AtriumGate><Day073PortalReadinessMobile/></AtriumGate>}
+import {Day073PortalExperienceMobile} from '../features/chokmah/Day073PortalExperienceMobile';
+export default function Day073Route(){return <AtriumGate><Day073PortalExperienceMobile/></AtriumGate>}
